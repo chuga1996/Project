b16dcd1f094df93e6767d1f745ffbd2b74b83b4c
Ba Manh Nguyen <manhnb05488@gmail.com>
2018-07-19 15:30:06 +0700

    Man hinh login, sign up

--- app/src/main/java/findingroom/cuonglm/poly/vn/findingroom/uis/ACLogin.java
+++ app/src/main/java/findingroom/cuonglm/poly/vn/findingroom/uis/ACLogin.java
@@ -1,15 +1,35 @@
 package findingroom.cuonglm.poly.vn.findingroom.uis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import findingroom.cuonglm.poly.vn.findingroom.R;
 
 public class ACLogin extends AppCompatActivity {
+    private EditText edtUser;
+    private EditText edtPassword;
+    private Button btnLogin;
+    private TextView tvSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edtUser = (EditText) findViewById(R.id.edt_user);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        tvSignup = (TextView) findViewById(R.id.tv_signup);
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ACLogin.this, SignupActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/findingroom/cuonglm/poly/vn/findingroom/uis/SignupActivity.java
@@ -0,0 +1,15 @@
+package findingroom.cuonglm.poly.vn.findingroom.uis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import findingroom.cuonglm.poly.vn.findingroom.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
