502465daad8830844e92bab518a226afc440d8b1
hathilam <lamhtph05479@fpt.edu.vn>
2018-07-23 12:24:49 +0700

    checkbox đã ht

--- app/src/main/java/findingroom/cuonglm/poly/vn/findingroom/uis/ACLogin.java
+++ app/src/main/java/findingroom/cuonglm/poly/vn/findingroom/uis/ACLogin.java
@@ -1,10 +1,12 @@
 package findingroom.cuonglm.poly.vn.findingroom.uis;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +17,7 @@ public class ACLogin extends AppCompatActivity {
     private EditText edtPassword;
     private Button btnLogin;
     private TextView tvSignup;
+    private CheckBox cbcheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class ACLogin extends AppCompatActivity {
         edtUser = (EditText) findViewById(R.id.edt_user);
         edtPassword = (EditText) findViewById(R.id.edt_password);
         btnLogin = (Button) findViewById(R.id.btn_login);
+        cbcheck=(CheckBox) findViewById(R.id.cbcheck);
         tvSignup = (TextView) findViewById(R.id.tv_signup);
 
         tvSignup.setOnClickListener(new View.OnClickListener() {
