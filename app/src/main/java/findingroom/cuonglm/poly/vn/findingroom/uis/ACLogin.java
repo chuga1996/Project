0bd656fc93fcd2d9d0c748422c7adbfb913a41f3
hathilam <lamhtph05479@fpt.edu.vn>
2018-07-24 14:51:34 +0700

    checkbox đã ht

--- app/src/main/java/findingroom/cuonglm/poly/vn/findingroom/uis/ACLogin.java
+++ app/src/main/java/findingroom/cuonglm/poly/vn/findingroom/uis/ACLogin.java
@@ -1,6 +1,7 @@
 package findingroom.cuonglm.poly.vn.findingroom.uis;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,12 +29,46 @@ public class ACLogin extends AppCompatActivity {
         btnLogin = (Button) findViewById(R.id.btn_login);
         cbcheck=(CheckBox) findViewById(R.id.cbcheck);
         tvSignup = (TextView) findViewById(R.id.tv_signup);
-
+        restoringPreferent();
         tvSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(ACLogin.this, SignupActivity.class));
             }
         });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savingPreferent();
+            }
+        });
+
+    }
+    public void savingPreferent(){
+        SharedPreferences preferences=getSharedPreferences("lam",MODE_PRIVATE);
+        SharedPreferences.Editor editor=preferences.edit();
+        String user=edtUser.getText().toString();
+        String password=edtPassword.getText().toString();
+        boolean check=cbcheck.isChecked();
+        if (!check){
+            editor.clear();
+        }else {
+            editor.putString("username",user);
+            editor.putString("password",password);
+            editor.putBoolean("savestatus",check);
+        }
+        editor.commit();
+
+    }
+    public void restoringPreferent(){
+        SharedPreferences preferences=getSharedPreferences("lam",MODE_PRIVATE);
+        boolean check=preferences.getBoolean("savestatus",false);
+        if (check){
+            String user=preferences.getString("username","");
+            String password=preferences.getString("password","");
+            edtUser.setText(user);
+            edtPassword.setText(password);
+        }
+        cbcheck.setChecked(check);
     }
 }
