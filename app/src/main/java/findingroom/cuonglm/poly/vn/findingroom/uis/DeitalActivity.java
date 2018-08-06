33cbc72a2c97fa14a4c1a787e69d0d8cb878908d
Ba Manh Nguyen <manhnb05488@gmail.com>
2018-08-06 14:08:16 +0700

    commit

--- app/src/main/java/findingroom/cuonglm/poly/vn/findingroom/uis/DeitalActivity.java
+++ app/src/main/java/findingroom/cuonglm/poly/vn/findingroom/uis/DeitalActivity.java
@@ -1,14 +1,12 @@
 package findingroom.cuonglm.poly.vn.findingroom.uis;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
